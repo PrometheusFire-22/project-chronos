e684db65dbc28f83c9187825e361ba061276ed61
Geoff Bevans <axiologycapital@gmail.com>
2026-01-09 16:48:33 +0000

    fix(web): enforce node protocol aliasing during worker bundling

---
+++ apps/web/build-worker.js
@@ -0,0 +1,23 @@
+import * as esbuild from 'esbuild';
+
+await esbuild.build({
+    entryPoints: ['.open-next/worker.js'],
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    format: 'esm',
+    target: 'node20',
+    outfile: '.open-next/_worker.js',
+    external: ['node:*', 'cloudflare:*'],
+    inject: ['./polyfills.js'],
+    plugins: [
+        {
+            name: 'node-prefix-alias',
+            setup(build) {
+                const builtins = ['assert', 'buffer', 'child_process', 'cluster', 'console', 'constants', 'crypto', 'dgram', 'dns', 'domain', 'events', 'fs', 'http', 'https', 'module', 'net', 'os', 'path', 'process', 'punycode', 'querystring', 'readline', 'repl', 'stream', 'string_decoder', 'sys', 'timers', 'tls', 'tty', 'url', 'util', 'vm', 'zlib'];
+                const filter = new RegExp(`^(${builtins.join('|')})$`);
+                build.onResolve({ filter }, args => ({ path: `node:${args.path}`, external: true }));
+            }
+        }
+    ]
+});
